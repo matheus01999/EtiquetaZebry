60118b1dd85ec0a7a8354c1104f31030931a0f5b
Matheus Rocha <matheus.rocha01999@outlook.com>
2024-07-16 21:08:41 -0300

    INICIO

---
+++ init.py
@@ -0,0 +1,24 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Welcome to GeekForGeeks")
+# Set geometry (widthxheight)
+root.geometry('500x500')
+
+#adding a label to the root window
+lbl = Label(root, text = "Selecione um Zebra")
+lbl.grid()
+
+# button widget with red color text
+# inside
+btn = Button(root, text = "IMPRIMIR")
+# set Button grid
+btn.grid(column=1, row=0)
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
