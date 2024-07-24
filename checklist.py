c85fec9788f0f25aa3c3ea3e12d7cfeefd7982ad
Matheus - Natura <matheus.rocha01999@outlook.com>
2024-07-24 08:01:23 -0300

    gerando os botoes de zebra pela com base na query ainda no inicia mas esat promissor

--- checklist.py
+++ checklist.py
@@ -1,37 +1,28 @@
-import tkinter as tk
+import sqlite3
+conn = sqlite3.connect("impressoras.db")
+cursor = conn.cursor()
 
-# Create the main window
-root = tk.Tk()
-root.geometry("400x250")  # Set window size
-root.title("Welcome to My App")  # Set window title
+a = cursor.execute("""SELECT * FROM printers
+                                          WHERE localidade = 'Rio Amazonas'""").fetchall()
 
-# Create a StringVar to associate with the label
-text_var = tk.StringVar()
-text_var.set("Hello, World!")
 
-# Create the label widget with all options
-label = tk.Label(root, 
-                 textvariable=text_var, 
-                 anchor=tk.CENTER,       
-                 bg="lightblue",      
-                 height=3,              
-                 width=30,              
-                 bd=3,                  
-                 font=("Arial", 16, "bold"), 
-                 cursor="hand2",   
-                 fg="red",             
-                 padx=15,               
-                 pady=15,                
-                 justify=tk.CENTER,    
-                 relief=tk.RAISED,     
-                 underline=0,           
-                 wraplength=250         
-                )
+ 
+a1 = []
+a2 = []
+a3 = []
+a4 = []
+a5 = []
 
-# Pack the label into the window
-label.pack(pady=20)  # Add some padding to the top
+indice = 1
 
-print(text_var.get())
+for i,num in enumerate(a):
+    variavel = 'a' + str(indice)
+    vars()[variavel].append(num)
 
-# Run the main event loop
-root.mainloop()
+    indice = indice + 1
+    if (indice >5):
+        indice = 1
+
+print(a1)
+print(a2)
+print(a3)
